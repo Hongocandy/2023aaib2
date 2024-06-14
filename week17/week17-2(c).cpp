2d86e19062dbce21311a400b783950433e155589
Hongocandy <145088471+Hongocandy@users.noreply.github.com>
2024-06-14 11:46:19 +0800

    week17

---
+++ week17/week17-2(c).cpp
@@ -0,0 +1,8 @@
+///week17-2c.cpp
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {///他會給你五行程式
+        sort(nums.begin(),nums.end());///就只要寫這一行就可以了
+        ///他會給你5行程式不要動它
+    }
+};
